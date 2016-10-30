0ccc3b034810dc843b05bb48bb04ecf1b9e9e19f
vedant8 <vedant8basu@gmail.com>
2016-10-30 18:50:26 +0530

    Add files via upload

--- dark.py
+++ dark.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import minimize
 from array import *
 import math
 import scipy
@@ -11,7 +12,7 @@ class plotting:
         events = [0.0] * 0
         background=np.zeros(40)
         signal=np.zeros(40)
-        m=20*0.1
+        m=20*3.53
         with open('recoilenergydata_EP219.csv') as csvfile:
             reader = csv.reader(csvfile, delimiter=',', quotechar='|')
             for row in reader:
@@ -29,11 +30,30 @@ class plotting:
         reading=signal+1000*np.exp(background)
         w = abs(energy[1]) - abs(energy[0])
         plt.bar(energy, events, width=w, alpha=0.5, align='center')
-        plt.bar(energy,reading,width=w, alpha=0.5, align='center' )
+        plt.bar(energy,reading,width=w,color='red', alpha=0.5, align='center' )
 
-        plt.title("Histogram for Probability Density of z")
-        plt.ylabel("Probability Density")
-        plt.xlabel("z")
-        plt.legend()
+        def line(param,d,energy):
+            s = param#slope parameter
+            xPred=[]
+            yPred=[]
+            for i in range(len(energy)):
+                xPred.append(-energy[i] / 10)
+                if ((energy[i] < 5) or (energy[i] > 25)):
+                    yPred.append(0)
+                elif ((energy[i] > 5) and (energy[i] < 15)):
+                    yPred.append(s * (energy[i] - 5))
+                elif ((energy[i] > 15) or (energy[i] < 25)):
+                    yPred.append(s * (25 - energy[i]))
+            read=yPred+1000*np.exp(xPred)
+            # Calculate negative log likelihood
+            LL = -np.sum(d*np.log(read)-read)
+            return(LL)
+
+        init=0.8
+        #x1 = np.linspace(0.0, 10.0, 5000.0)
+        #plt.plot(x1,line(x1,events,energy)
+        results = minimize(line, init,args=(events,energy), method='Nelder-Mead')  # minimization
+        print results.x  # best fit params
+
         plt.show()
 plotting().panda()
