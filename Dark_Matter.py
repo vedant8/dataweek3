db2574438b5c0741d783149b14e5215c62485406
Parth Jatakia <parth.jatakia@gmail.com>
2016-11-02 23:39:27 +0530

    Update Dark_Matter.py
    Completed the code, improved the efficency, made it readable and wrote the comments

--- Dark_Matter.py
+++ Dark_Matter.py
@@ -1,10 +1,5 @@
 import numpy as np
 from scipy.optimize import minimize
-from array import *
-import math
-import scipy
-from scipy.stats import binom
-import csv
 import matplotlib.pyplot as plt
 
 energy = [0.0] * 0
@@ -22,86 +17,113 @@ for line in fhand:
     events.append(float(p[1]))
     background = np.divide(np.multiply(energy, -1), 10)
 
+# Deciding the width of the histogram
 w = abs(energy[1]) - abs(energy[0])
 
+# Plotting the Histogram of the given data
 plt.bar(energy, events, width=w, alpha=0.5, align='center')
+plt.title("Histogram of the Events Measured")
+plt.xlabel("Energy")
+plt.ylabel("Frequency of the events measured")
 plt.show()
 
+# Plotting the Histogram of the mean background events
 plt.bar(energy, 1000 * np.exp(background), width=w, color='red', alpha=0.5, align='center')
+plt.title("Histogram of the mean background events")
+plt.xlabel("Energy")
+plt.ylabel("Frequency of the mean background events")
 plt.show()
 
-def signalMean (sigma):
+# Function to calculate mean reading events for a given value of sigma
+def readingMean(sigma):
     for i in range(len(energy)):
         if (energy[i] < 5) or (energy[i] > 25):
             signal[i] = 0
         elif (energy[i] > 5) and (energy[i] < 15):
-            signal[i] = sigma* 20 * (energy[i] - 5)
+            signal[i] = sigma * 20 * (energy[i] - 5)
         elif (energy[i] > 15) or (energy[i] < 25):
             signal[i] = sigma * 20 * (25 - energy[i])
     reading = signal + 1000 * np.exp(background)
     return reading
 
-plt.bar(energy, signalMean(0.01), width=w, color='red', alpha=0.5, align='center')
+# Plotting histogram for different value of sigma
+plt.bar(energy, readingMean(0.01), width=w, color='red', alpha=0.5, align='center')
+plt.title("Histogram of the Mean Events with sigma = 0.01")
+plt.xlabel("Energy")
+plt.ylabel("Frequency of the mean events")
 plt.show()
-plt.bar(energy, signalMean(0.1), width=w, color='red', alpha=0.5, align='center')
+
+plt.bar(energy, readingMean(0.1), width=w, color='red', alpha=0.5, align='center')
+plt.title("Histogram of the Mean Events with sigma = 0.1")
+plt.xlabel("Energy")
+plt.ylabel("Frequency of the mean events")
 plt.show()
-plt.bar(energy, signalMean(1), width=w, color='red', alpha=0.5, align='center')
+
+plt.bar(energy, readingMean(1), width=w, color='red', alpha=0.5, align='center')
+plt.title("Histogram of the Mean Events with sigma = 1")
+plt.xlabel("Energy")
+plt.ylabel("Frequency of the mean events")
 plt.show()
-plt.bar(energy, signalMean(10), width=w, color='red', alpha=0.5, align='center')
+
+plt.bar(energy, readingMean(10), width=w, color='red', alpha=0.5, align='center')
+plt.title("Histogram of the Mean Events with sigma = 10")
+plt.xlabel("Energy")
+plt.ylabel("Frequency of the mean events")
 plt.show()
-plt.bar(energy, signalMean(100), width=w, color='red', alpha=0.5, align='center')
+
+plt.bar(energy, readingMean(100), width=w, color='red', alpha=0.5, align='center')
+plt.title("Histogram of the Mean Events with sigma = 100")
+plt.xlabel("Energy")
+plt.ylabel("Frequency of the mean events")
 plt.show()
 
 
 #################################################################################################
+#                              Log Likelihood Function and Calculation                          #
+#################################################################################################
 
+# Calculates the negative of the log likelihood function
 def line(param, d, energy):
     s = param
-    yPred = []
-    xPred = np.divide(energy, 10)
-    for i in range(len(energy)):
-        if (energy[i] < 5) or (energy[i] > 25):
-            yPred.append(0)
-        elif (energy[i] > 5) and (energy[i] < 15):
-            yPred.append(s * 20 * (energy[i] - 5))
-        elif (energy[i] > 15) or (energy[i] < 25):
-            yPred.append(s * 20 * (25 - energy[i]))
-    read = yPred + np.multiply(1000, np.exp(-xPred))
+    read = readingMean(s)
     # Calculate negative log likelihood
     LL = -np.sum(d * np.log(read) - read)
     return LL
 
-sigma = np.arange(0, 1, 0.01)
+# Plotting the log likelihood function between 0 to 100
+sigma = np.arange(0, 10, 0.01)
 for i in range(len(sigma)):
-    plt.plot(sigma[i], line(sigma[i], events, energy),'ro')
+    plt.plot(sigma[i], -line(sigma[i], events, energy), 'ro')
+plt.title("Log Likelihood function for the theory parameter sigma")
+plt.xlabel("Theory parameter sigma")
+plt.ylabel("Log Likelihood Value")
 plt.show()
 
-
+# Calculating the maximum Log likelihood
 init = np.array([0.5])
-# x1 = np.linspace(0.0, 10.0, 5000.0
-# plt.plot(x1,line(x1,events,energy)
-results = minimize(line, init, args=(events, energy), method='Nelder-Mead')  # minimization
-print results.x  # best fit params
-LLmin = line(results.x,events,energy)
-print LLmin
-OneSigma = np.array([0,0])
-k = 0
-x = []
+results = minimize(line, init, args=(events, energy), method='Nelder-Mead')      # minimization
+print "The sigma at which Log Likelihood function is maximum is at %s"%results.x  # best fit parameter
 
-for i in range(0, int(10**5*LLmin)):
-    sigma = float(i)/10**5
-    x.append(line(sigma, events, energy))
-
-OneSigma[0] = min(x, key=lambda x: abs(x-(LLmin+0.5)))
-
+# Calculating the 1sigma interval
+LLmin = -line(results.x, events, energy)
+OneSigma = np.array([0.0, 0.0])
+LLOneSigma = float('inf')
+# Calculating the lower bound
+for i in range(0, int(10 ** 5 * results.x)):
+    sigma = float(i) / 10 ** 5
+    x = -line(sigma, events, energy)
+    if abs(x - (LLmin - 0.5)) < LLOneSigma:
+        LLOneSigma = abs(x - (LLmin - 0.5))
+        OneSigma[0] = sigma
+# Calculating the upper bound
 y = []
+LLOneSigma = float('inf')
+for i in range(int(10 ** 5 * results.x), 10 ** 5):
+    sigma = float(i) / 10 ** 5
+    x = -line(sigma, events, energy)
+    if abs(x - (LLmin - 0.5)) < LLOneSigma:
+        LLOneSigma = abs(x - (LLmin - 0.5))
+        OneSigma[1] = sigma
 
-for i in range(int(10**5*LLmin),10**5):
-    sigma = float(i)/10**5
-    y.append(line(sigma, events, energy))
-
-OneSigma[1] = min(x, key=lambda x: abs(x-(LLmin+0.5)))
-
-print OneSigma
-
+print "The 1 sigma interval is ( %s, %s )" %(OneSigma[0], OneSigma[1])
 
